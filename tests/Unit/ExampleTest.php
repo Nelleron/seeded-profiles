ca78faa730ca695b5ba5ffab926b6b3509057489
Vladislav <vlad.vladisllav@gmail.com>
2026-04-14 16:00:18 +0300

    feat: setup database schema, models and CI

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use Codeception\Test\Unit;
+
+class ExampleTest extends Unit
+{
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}
