f428a11ecb89a8e430598bbe4113fdd9a1ce4469
Vladislav <vlad.vladisllav@gmail.com>
2026-04-14 16:03:40 +0300

    feat: setup database schema, models and CI

---
+++ tests/Functional/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Functional;
+
+use Codeception\Test\Functional;
+
+class ExampleTest extends Functional
+{
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
